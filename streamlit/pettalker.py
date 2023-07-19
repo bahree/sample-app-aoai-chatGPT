013564d7d89710064a8ccec543fde821171f8f6f
Amit Bahree <github@desigeek.com>
2023-07-19 06:16:27 -0700

    adding basics to streamlit app

--- streamlit/pettalker.py
+++ streamlit/pettalker.py
@@ -1,14 +1,50 @@
 # Importing required packages
 import streamlit as st
 import openai
+import os
 
 from typing import List
 
 # pip install streamlit-chat 
 from streamlit_chat import message
 
-# Defining the app title
+#pip install python-dotenv
+from dotenv import load_dotenv
+
+# Loading the .env file
+load_dotenv()
+
+# Get API key from environment variable
+#AOAI_KEY = os.getenv("OPENAI_API_KEY")  
+
+# Set up OpenAI API credentials
+openai.api_type = "azure"
+openai.api_base = 'https://xxxxxx.openai.azure.com/'
+openai.api_version = "2023-03-15-preview"
+#openai.api_key = OPENAI_API_KEY
+
+# Defining the app title and layout
 st.title("PetTalker- powered by ChatGPT")
+language = st.selectbox("Select Language", ["Python", "JavaScript"])
+code_input = st.text_area("Enter code to explain")
+st.button("Explain")
+
+# Temperature and token slider
+temperature = st.sidebar.slider(
+    "Temperature",
+    min_value=0.0,
+    max_value=1.0,
+    value=0.5,
+    step=0.1
+)
+
+tokens = st.sidebar.slider(
+    "Tokens",
+    min_value=64,
+    max_value=2048,
+    value=256,
+    step=64
+)
 
 
 def chat_completion(ai_model: str, messages: List[dict]) -> dict:
